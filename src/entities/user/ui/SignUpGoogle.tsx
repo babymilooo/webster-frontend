bb618b5155b992929a262dde0662d5ca9aa9e3f9
DarunkaKa <shmetterling.dk@gmail.com>
2024-05-14 18:13:21 +0300

    update SignUpGoogle

--- src/entities/user/ui/SignUpGoogle.tsx
+++ src/entities/user/ui/SignUpGoogle.tsx
@@ -1,8 +1,24 @@
 import { Img } from 'react-image';
+import { signUpGoogle } from '../api/signUpGoogle';
+import { useState, useEffect } from 'react';
 
 const SignUpGoogle = () => {
+    const [redirectUrl, setRedirectUrl] = useState('');
+
+    const handleSignUp = async () => {
+        const data = await signUpGoogle();
+        console.log(data);
+        if (data)
+            setRedirectUrl(data);
+    };
+
+    useEffect(() => {
+        if (redirectUrl)
+            window.location.href = redirectUrl;
+    }, [redirectUrl]);
+
     return (
-        <button className=" mt-8 flex w-1/2  items-center justify-center gap-2 rounded-lg border bg-white p-[9px] text-sm text-black">
+        <button className=" mt-8 flex w-1/2  items-center justify-center gap-2 rounded-lg border bg-white p-[9px] text-sm text-black" onClick={handleSignUp}>
             <Img
                 src="../src/public/google.svg"
                 alt="My Image"
