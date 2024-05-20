4efa1195bbc9d2ce10da4cca6caffaf1726dec46
mtkachov <mtkachov@stud.khpi.ucode-connect.study>
2024-05-20 20:06:58 +0200

    refactor rect Instrument

---
+++ src/entities/project/lib/Instruments/Rect.ts
@@ -0,0 +1,83 @@
+import Konva from 'konva';
+import { IInstuments } from './types';
+import { useProjectStore } from '../../model/projectStore';
+import { setOffDragable } from '../setDragable';
+import { clearAllSelection } from '../clearAllSelection';
+
+export class RectInstrument implements IInstuments {
+    type = 'Rect';
+    isDrawing = false;
+    rect: Konva.Rect | null = null;
+    startPos: { x: number; y: number } | null = null;
+
+    applyToStage() {
+        const stage = useProjectStore.getState().stage;
+
+        if (stage) {
+            setOffDragable();
+            stage.off('pointerdown pointermove pointerup');
+            stage.on('pointerdown', this.onPointerDown);
+            stage.on('pointermove', this.onPointerMove);
+            stage.on('pointerup', this.onPointerUp);
+        }
+    }
+
+    onPointerDown() {
+        const layer = useProjectStore.getState().selectedLayer;
+        const stage = useProjectStore.getState().stage;
+        if (!layer || !stage) return;
+        const transformer = layer.findOne('Transformer') as Konva.Transformer;
+
+        const pos = stage.getPointerPosition();
+        if (!pos) return;
+
+        const newRect = new Konva.Rect({
+            x: pos.x,
+            y: pos.y,
+            width: 0,
+            height: 0,
+            fill: 'none',
+            stroke: 'black',
+            strokeWidth: 4,
+            draggable: false,
+        });
+
+        newRect.on('click tap', () => {
+            clearAllSelection(stage);
+            transformer.nodes([newRect]);
+        });
+
+        layer.add(newRect);
+        this.rect = newRect;
+        this.startPos = pos;
+        this.isDrawing = true;
+    }
+    onPointerMove() {
+        const layer = useProjectStore.getState().selectedLayer;
+        const stage = useProjectStore.getState().stage;
+        if (!layer || !stage) return;
+        if (!this.isDrawing || !this.rect || !this.startPos) return;
+
+        const pos = stage.getPointerPosition();
+        if (!pos) return;
+
+        const newX = Math.min(pos.x, this.startPos.x);
+        const newY = Math.min(pos.y, this.startPos.y);
+        const newWidth = Math.abs(pos.x - this.startPos.x);
+        const newHeight = Math.abs(pos.y - this.startPos.y);
+
+        this.rect.setAttrs({
+            x: newX,
+            y: newY,
+            width: newWidth,
+            height: newHeight,
+        });
+
+        layer.batchDraw();
+    }
+    onPointerUp() {
+        this.isDrawing = false;
+        this.rect = null;
+        this.startPos = null;
+    }
+}
--- src/entities/project/ui/AddRect.tsx
+++ src/entities/project/ui/AddRect.tsx
@@ -1,103 +1,23 @@
-import Konva from 'konva';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect } from 'react';
 import { useProjectStore } from '@/entities/project';
+import { RectInstrument } from '../lib/Instruments/Rect';
 
-type AddRectProps = {
-    stageRef: React.RefObject<Konva.Stage>;
-    clearAllSelection: (stage?: Konva.Stage | null) => void;
-};
-
-export const AddRect: React.FC<AddRectProps> = ({
-    stageRef,
-    clearAllSelection,
-}) => {
-    const [isDrawing, setIsDrawing] = useState(false);
-    const [rect, setRect] = useState<Konva.Rect | null>(null);
-    const layerRef = useRef<Konva.Layer | null>(null);
-    const [startPos, setStartPos] = useState<{ x: number; y: number } | null>(
-        null,
-    );
+export const AddRect: React.FC = () => {
     const state = useProjectStore((state) => state.state);
     const drawState = useProjectStore((state) => state.drawState);
     useEffect(() => {
-        if (!stageRef.current) return;
-
-        const stage = stageRef.current;
+        const stage = useProjectStore.getState().stage;
+        if (!stage) return;
 
         if (state === 'CreateFigure' && drawState === 'Rect') {
-            const handleMouseDown = () => {
-                const layer = useProjectStore.getState().selectedLayer;
-                if (!layer) return;
-                const transformer = layer.findOne(
-                    'Transformer',
-                ) as Konva.Transformer;
-
-                layerRef.current = layer;
-                const pos = stage.getPointerPosition();
-                if (!pos) return;
-
-                const newRect = new Konva.Rect({
-                    x: pos.x,
-                    y: pos.y,
-                    width: 0,
-                    height: 0,
-                    fill: 'none',
-                    stroke: 'black',
-                    strokeWidth: 4,
-                    draggable: true,
-                });
-
-                newRect.on('click tap', () => {
-                    clearAllSelection(stageRef.current);
-                    transformer.nodes([newRect]);
-                });
-
-                layer.add(newRect);
-                setRect(newRect);
-                setStartPos(pos);
-                setIsDrawing(true);
-            };
-
-            const handleMouseMove = () => {
-                if (!isDrawing || !rect || !startPos) return;
-
-                const pos = stage.getPointerPosition();
-                if (!pos) return;
-
-                const newX = Math.min(pos.x, startPos.x);
-                const newY = Math.min(pos.y, startPos.y);
-                const newWidth = Math.abs(pos.x - startPos.x);
-                const newHeight = Math.abs(pos.y - startPos.y);
-
-                rect.setAttrs({
-                    x: newX,
-                    y: newY,
-                    width: newWidth,
-                    height: newHeight,
-                });
-
-                layerRef.current?.batchDraw();
-            };
-
-            const handleMouseUp = () => {
-                setIsDrawing(false);
-                setRect(null);
-                setStartPos(null);
-            };
-
-            stage.off('pointerdown pointermove pointerup');
-            stage.on('pointerdown', handleMouseDown);
-            stage.on('pointermove', handleMouseMove);
-            stage.on('pointerup', handleMouseUp);
+            const instrument = new RectInstrument();
+            instrument.applyToStage();
 
             return () => {
                 stage.off('pointerdown pointermove pointerup');
-                // stage.off('pointerdown', handleMouseDown);
-                // stage.off('pointermove', handleMouseMove);
-                // stage.off('pointerup', handleMouseUp);
             };
         }
-    }, [stageRef, isDrawing, rect, clearAllSelection, state, drawState]);
+    }, [state, drawState]);
 
     return null;
 };
