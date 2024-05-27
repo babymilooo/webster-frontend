ce4135ec51dfde729ade27ab57f7e95d7d6852f5
babymilooo <Kyrylo.Yehorov@cit.khpi.edu.ua>
2024-05-27 19:57:40 +0300

    features: add color correction dialog to preview

--- src/entities/project/ui/ColorCorrection.tsx
+++ src/entities/project/ui/ColorCorrection.tsx
@@ -7,7 +7,10 @@ import {
     DialogTrigger,
 } from '@/shared/ui/dialog';
 
-export const ColorCorrection = ({}) => {
+export const ColorCorrection = ({SelectedShape}: {SelectedShape: any}) => {
+    const src = SelectedShape?.getAttr('src');
+    if (!src) return null;
+
     return (
         <Dialog>
             <DialogTrigger className="py-2 px-4 rounded-lg text-center hover:bg-secondary">
@@ -17,9 +20,7 @@ export const ColorCorrection = ({}) => {
                 <DialogHeader>
                     <DialogTitle>Edit your photo</DialogTitle>
                     <DialogDescription>
-                        This action cannot be undone. This will permanently
-                        delete your account and remove your data from our
-                        servers.
+                        <img src={src} alt="preview" className="w-full" />
                     </DialogDescription>
                 </DialogHeader>
             </DialogContent>
