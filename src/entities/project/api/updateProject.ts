1cab62a5270d5a06ece213c15e56b4e9a1f5448a
babymilooo <Kyrylo.Yehorov@cit.khpi.edu.ua>
2024-06-07 23:25:02 +0300

    Fix: update title

--- src/entities/project/api/updateProject.ts
+++ src/entities/project/api/updateProject.ts
@@ -1,9 +1,8 @@
 import $api from '@/app/http/axios';
 export const updateProject = async (title: string, id: string) => {
-    const data = { title };
 
     const response = await $api.patch(`/project/${id}`, {
-        data,
+        title,
     });
     return response.data;
 };
