0e896523ae06a3241b97f942e8a6aa26d2184928
DarunkaKa <shmetterling.dk@gmail.com>
2024-06-09 18:40:24 +0300

    added settings

--- src/pages/project/ProjectLayout.tsx
+++ src/pages/project/ProjectLayout.tsx
@@ -1,19 +1,15 @@
 import ProjectLeftSidebar from '@/widgets/ProjectLeftSidebar';
 import { ProjectNavbar } from '@/widgets/ProjectNavbar';
 import ProjectRightSidebar from '@/widgets/ProjectRightSidebar';
-import { ScaleBar } from '@/widgets/project/scaleBar';
 import { ReactNode } from 'react';
 
 const ProjectLayout = ({ children }: { children: ReactNode }) => {
     return (
-        <div className="h-full w-full pt-[50px] pr-[300px]">
+        <div className="h-full w-full pt-[50px]">
             <ProjectNavbar />
             <ProjectLeftSidebar />
             <ProjectRightSidebar />
-            <div className="flex h-full w-full items-center pt-[100px] pb-[100px]">
-                {children}
-            </div>
-            <ScaleBar />
+            <div className="h-full w-full pl-[80px] pr-[300px] pt-[50px]">{children}</div>
         </div>
     );
 };
