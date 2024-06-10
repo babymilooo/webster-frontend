93148ec698c0df26412a02351b84419bb1b5222c
mtkachov <mtkachov@stud.khpi.ucode-connect.study>
2024-06-10 12:08:10 +0200

    working carousel tiles

--- src/widgets/Carousel.tsx
+++ src/widgets/Carousel.tsx
@@ -10,22 +10,70 @@ import CustomCard from './CustomCard';
 import './Carousel.css';
 
 const cards = [
-    { imageSrc: '../src/public/carousel-logo.png', caption: 'Logo', dimensions: '500 x 500 px' },
-    { imageSrc: '../src/public/carousel-whiteboard.png', caption: 'Whiteboard', dimensions: 'Unlimited' },
-    { imageSrc: '../src/public/carousel-doc.png', caption: 'Doc', dimensions: 'Autosize' },
-    { imageSrc: '../src/public/carousel-bookmark.png', caption: 'Bookmark', dimensions: '2 x 6 in' },
-    { imageSrc: '../src/public/carousel-business-card.png', caption: 'Business Card', dimensions: '3.5 x 2 in' },
-    { imageSrc: '../src/public/carousel-post.png', caption: 'Instagram Post', dimensions: '1080 x 1080 px' },
-    { imageSrc: '../src/public/carousel-flyer.png', caption: 'Flyer A4', dimensions: '21 x 29.7 cm' },
-    { imageSrc: '../src/public/carousel-postcard.png', caption: 'Postcard', dimensions: '148 x 105 mm' },
-    { imageSrc: '../src/public/carousel-poster.png', caption: 'Posters', dimensions: '42 x 59.4 cm' },
-    { imageSrc: '../src/public/carousel-cover-image.jpg', caption: 'Cover Image', dimensions: '2000 x 600 px' },
+    {
+        imageSrc: '../src/public/carousel-logo.png',
+        caption: 'Logo',
+        dimensions: '500 x 500 px',
+        height: 500,
+        width: 500,
+    },
+    // { imageSrc: '../src/public/carousel-whiteboard.png', caption: 'Whiteboard', dimensions: 'Unlimited' },
+    // { imageSrc: '../src/public/carousel-doc.png', caption: 'Doc', dimensions: 'Autosize' },
+    {
+        imageSrc: '../src/public/carousel-bookmark.png',
+        caption: 'Bookmark',
+        dimensions: '2 x 6 in',
+        height: 6 * 96,
+        width: 2 * 96,
+    },
+    {
+        imageSrc: '../src/public/carousel-business-card.png',
+        caption: 'Business Card',
+        dimensions: '3.5 x 2 in',
+        height: 3.5 * 96,
+        width: 2 * 96,
+    },
+    {
+        imageSrc: '../src/public/carousel-post.png',
+        caption: 'Instagram Post',
+        dimensions: '1080 x 1080 px',
+        height: 1080,
+        width: 1080,
+    },
+    {
+        imageSrc: '../src/public/carousel-flyer.png',
+        caption: 'Flyer A4',
+        dimensions: '21 x 29.7 cm',
+        height: 29.7 * 37.795,
+        width: 21 * 37.795,
+    },
+    {
+        imageSrc: '../src/public/carousel-postcard.png',
+        caption: 'Postcard',
+        dimensions: '148 x 105 mm',
+        height: 105 * 3.779527559,
+        width: 148 * 3.779527559,
+    },
+    {
+        imageSrc: '../src/public/carousel-poster.png',
+        caption: 'Posters',
+        dimensions: '42 x 59.4 cm',
+        height: 59.4 * 37.795,
+        width: 42 * 37.795,
+    },
+    {
+        imageSrc: '../src/public/carousel-cover-image.jpg',
+        caption: 'Cover Image',
+        dimensions: '2000 x 600 px',
+        height: 600,
+        width: 2000,
+    },
 ];
 
 const Carousel: React.FC = () => {
     return (
         <div className="carousel-container">
-            <ShadCarousel className="w-full relative">
+            <ShadCarousel className="relative w-full">
                 <CarouselContent className="flex">
                     {cards.map((card, index) => (
                         <CarouselItem key={index} className="carousel-item">
@@ -33,6 +81,8 @@ const Carousel: React.FC = () => {
                                 imageSrc={card.imageSrc}
                                 caption={card.caption}
                                 dimensions={card.dimensions}
+                                width={card.width}
+                                height={card.height}
                             />
                         </CarouselItem>
                     ))}
--- src/widgets/CustomCard.tsx
+++ src/widgets/CustomCard.tsx
@@ -1,25 +1,279 @@
-import React from 'react';
-import { Card as ShadCard, CardContent as ShadCardContent } from '@/shared/ui/card';
+import React, { ChangeEvent, useRef, useState } from 'react';
+import {
+    Card as ShadCard,
+    CardContent as ShadCardContent,
+} from '@/shared/ui/card';
 import './CustomCard.css';
+import { useNavigate } from 'react-router-dom';
+import { useUserStore } from '@/entities/user';
+import {
+    createProject,
+    updatePicture,
+    useProjectStore,
+} from '@/entities/project';
+import { useInitProjectStore } from '@/entities/project/model/initProjectStore';
+import {
+    Dialog,
+    DialogContent,
+    DialogTitle,
+    DialogTrigger,
+} from '@/shared/ui/dialog';
+import { Input } from '@/shared/ui/input';
+import { DialogClose } from '@radix-ui/react-dialog';
+import { Button } from '@/shared/ui/button';
+import { Label } from '@/shared/ui/label';
 
-const CustomCard = ({ imageSrc, caption, dimensions }) => (
-    <div className="p-1 custom-card">
-        <ShadCard className="relative overflow-hidden w-full h-full">
-            <ShadCardContent className="flex items-center justify-center p-6 relative w-full h-full gradient-background">
-                <div className="w-full text-center">
-                    <div className="image-container">
-                        <img src={imageSrc} alt={caption} className="image" />
-                    </div>
-                    <div className="mt-2 text-sm">{caption}</div>
-                    <div className="text-xs text-gray-500">{dimensions}</div>
+const CustomCard = ({
+    imageSrc,
+    caption,
+    dimensions,
+    height,
+    width,
+}: {
+    imageSrc: string;
+    caption: string;
+    dimensions: string;
+    height: number;
+    width: number;
+}) => {
+    const imageInputRef = useRef<HTMLInputElement | null>(null);
+    const backgroundImageInputRef = useRef<HTMLInputElement | null>(null);
+    const navigate = useNavigate();
+    const setProjects = useUserStore((state) => state.setProjects);
+    const setProject = useProjectStore((state) => state.setProject);
+    const projects = useUserStore((state) => state.projects);
+    const isLogin = useUserStore((state) => state.isLogin);
+
+    const [
+        setStartImage,
+        setStartBackgroundImage,
+        setStartJSON,
+        setWidth,
+        setHeight,
+        resetStore,
+    ] = useInitProjectStore((state) => [
+        state.setStartingImage,
+        state.setStartingBackgroundImage,
+        state.setSerializedJSON,
+        state.setWidth,
+        state.setHeight,
+        state.resetStore,
+    ]);
+
+    const [initWidth, initHeight] = useInitProjectStore((state) => [
+        state.width,
+        state.height,
+    ]);
+
+    const [title, setTitle] = useState('');
+
+    const handleSelectImageForProject = (e: ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (!file) return;
+        const reader = new FileReader();
+        reader.onload = async (e) => {
+            if (!title || title.trim().length == 0) return;
+            const projData = await createProject(title);
+            // console.log(projData);
+
+            let data: any = null;
+            if (isLogin) {
+                data = (await updatePicture(file, projData._id)).image;
+            } else {
+                data = e.target?.result;
+                if (typeof data !== 'string') return;
+            }
+            // console.log(data);
+
+            const img = new window.Image();
+            img.src = data;
+            img.onload = async () => {
+                resetStore();
+                setHeight(img.height);
+                setWidth(img.width);
+                setStartImage(data);
+                setStartBackgroundImage(null);
+                img.remove();
+                if (!isLogin) {
+                    navigate('/projects/tmp');
+                    return;
+                }
+                if (projData) {
+                    const formatedProjects = projects
+                        ? [...projects, projData]
+                        : [projData];
+                    setProjects(formatedProjects as []);
+                    setProject(projData);
+                }
+
+                navigate(`/projects/${projData._id}`);
+            };
+        };
+        reader.readAsDataURL(file);
+    };
+
+    const handleSelectbackgroundImageForProject = (
+        e: ChangeEvent<HTMLInputElement>,
+    ) => {
+        const file = e.target.files?.[0];
+        if (!file) return;
+        const reader = new FileReader();
+        reader.onload = async (e) => {
+            if (!title || title.trim().length == 0) return;
+            const projData = await createProject(title);
+            let data: any = null;
+            if (isLogin) {
+                data = (await updatePicture(file, projData._id)).image;
+            } else {
+                data = e.target?.result;
+                if (typeof data !== 'string') return;
+            }
+            const img = new window.Image();
+            img.src = data;
+            img.onload = async () => {
+                resetStore();
+                setHeight(img.height);
+                setWidth(img.width);
+                setStartImage(null);
+                setStartBackgroundImage(data);
+                img.remove();
+                if (!isLogin) {
+                    navigate('/projects/tmp');
+                    return;
+                }
+                if (projData) {
+                    const formatedProjects = projects
+                        ? [...projects, projData]
+                        : [projData];
+                    setProjects(formatedProjects as []);
+                    setProject(projData);
+                }
+
+                navigate(`/projects/${projData._id}`);
+            };
+        };
+        reader.readAsDataURL(file);
+    };
+
+    const handleCreate = async () => {
+        // resetStore();
+        if (!isLogin) {
+            navigate('/projects/tmp');
+            return;
+        }
+        if (!title || title.trim().length == 0) return;
+        const data = await createProject(title);
+        if (data) {
+            const formatedProjects = projects ? [...projects, data] : [data];
+            setProjects(formatedProjects as []);
+            setProject(data);
+        }
+
+        navigate(`/projects/${data._id}`);
+    };
+    return (
+        <Dialog>
+            <DialogTrigger
+                onClick={() => {
+                    setWidth(width);
+                    setHeight(height);
+                }}
+            >
+                <div className="custom-card p-1">
+                    <ShadCard className="relative h-full w-full overflow-hidden">
+                        <ShadCardContent className="gradient-background relative flex h-full w-full items-center justify-center p-6">
+                            <div className="w-full text-center">
+                                <div className="image-container">
+                                    <img
+                                        src={imageSrc}
+                                        alt={caption}
+                                        className="image"
+                                    />
+                                </div>
+                                <div className="mt-2 text-sm">{caption}</div>
+                                <div className="text-xs text-gray-500">
+                                    {dimensions}
+                                </div>
+                            </div>
+                            <div className="overlay absolute left-0 top-0 flex h-full w-full flex-col items-center justify-center bg-black bg-opacity-60 opacity-0 transition-opacity duration-300">
+                                <span className="text-white">Create blank</span>
+                                <span className="mt-1 text-sm text-white">
+                                    {dimensions}
+                                </span>
+                            </div>
+                        </ShadCardContent>
+                    </ShadCard>
                 </div>
-                <div className="overlay absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center bg-black bg-opacity-60 opacity-0 transition-opacity duration-300">
-                    <span className="text-white">Create blank</span>
-                    <span className="text-white text-sm mt-1">{dimensions}</span>
+            </DialogTrigger>
+            <DialogContent className="w-3/5">
+                <DialogTitle>Create Project</DialogTitle>
+                <div className="flex flex-col gap-4">
+                    <Input
+                        value={title}
+                        required
+                        type="text"
+                        placeholder="Project Title"
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                    <Label>Width</Label>
+                    <Input
+                        value={initWidth}
+                        required
+                        type="number"
+                        placeholder="Project Width"
+                        onChange={(e) => setWidth(Number(e.target.value))}
+                    />
+                    <Label>Height</Label>
+                    <Input
+                        value={initHeight}
+                        required
+                        type="number"
+                        placeholder="Project Height"
+                        onChange={(e) => setHeight(Number(e.target.value))}
+                    />
+                    <button
+                        className="rounded-lg px-4 py-2 text-center hover:bg-secondary disabled:text-gray-500"
+                        onClick={handleCreate}
+                        disabled={!title || title.trim().length == 0}
+                    >
+                        Create New Project
+                    </button>
+
+                    <button
+                        className="rounded-lg px-4 py-2 text-center hover:bg-secondary disabled:text-gray-500"
+                        onClick={() => imageInputRef.current?.click()}
+                        disabled={!title || title.trim().length == 0}
+                    >
+                        Create Project with image
+                    </button>
+                    <input
+                        type="file"
+                        accept="image/*"
+                        onChange={handleSelectImageForProject}
+                        className="hidden"
+                        ref={imageInputRef}
+                    />
+                    <button
+                        className="rounded-lg px-4 py-2 text-center hover:bg-secondary disabled:text-gray-500"
+                        onClick={() => backgroundImageInputRef.current?.click()}
+                        disabled={!title || title.trim().length == 0}
+                    >
+                        Create Project with background image
+                    </button>
+                    <input
+                        type="file"
+                        accept="image/*"
+                        onChange={handleSelectbackgroundImageForProject}
+                        className="hidden"
+                        ref={backgroundImageInputRef}
+                    />
                 </div>
-            </ShadCardContent>
-        </ShadCard>
-    </div>
-);
+                <DialogClose>
+                    <Button>Close</Button>
+                </DialogClose>
+            </DialogContent>
+        </Dialog>
+    );
+};
 
 export default CustomCard;
