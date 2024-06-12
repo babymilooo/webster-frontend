b298c9b82adabbb24a220e98398330a6a50e3bc4
babymilooo <Kyrylo.Yehorov@cit.khpi.edu.ua>
2024-06-12 15:06:59 +0300

    Fix: new style for gallery

--- src/widgets/SavedProjectTile.tsx
+++ src/widgets/SavedProjectTile.tsx
@@ -49,74 +49,97 @@ export const SavedProjectTile: FC<SavedProjectTileProps> = ({
     };
 
     return (
-        <div className="flex max-h-[300px] flex-col items-center justify-between rounded p-2">
-            <div
-                className="border-[3px] border-orange-950 p-2 shadow-lg"
-                onClick={() => navigate(`/projects/${project._id}`)}
-            >
-                <img
-                    className=" border"
-                    src={`${API_URL}/project/${project._id}/thumbnail`}
-                    alt={`Preview ${project.title}`}
-                />
-            </div>
-
-            <div className="relative w-full p-2 text-center">
-                <div className="group relative flex items-center justify-center">
-                    {isEditing ? (
-                        <div className="flex items-center">
-                            <input
-                                type="text"
-                                value={newTitle}
-                                onChange={handleTitleChange}
-                                // onBlur={handleTitleBlur}
-                                onKeyDown={(e) => {
-                                    if (e.key === 'Enter') {
-                                        handleTitleSave();
-                                    } else if (e.key === 'Escape') {
-                                        handleTitleBlur();
-                                    }
-                                }}
-                                className="w-[100px] text-center"
-                                autoFocus
-                            />
-                            <CheckIcon
-                                className="absolute right-0 h-5 w-5"
-                                onClick={handleTitleSave}
-                            />
-                            <Cross1Icon
-                                className="absolute right-[-20px] h-3 w-3"
-                                onClick={handleTitleBlur}
-                            />
-                        </div>
-                    ) : (
-                        <span className="text-nowrap">{project.title}</span>
-                    )}
-                    <div className="absolute right-0 ml-2 opacity-0 transition-opacity group-hover:opacity-100">
-                        <div className="cursor-pointer" onClick={onMenuToggle}>
-                            {isEditing ? null : ( // Hide edit icon when editing
-                                <PenIcon className="h-3 w-3" />
-                            )}
-                        </div>
-                        {isMenuVisible && (
-                            <div
-                                className="w-21 absolute right-0 top-full z-10  rounded-lg border bg-background text-sm text-foreground
-                            shadow-md "
-                            >
-                                <button
-                                    className="block w-full p-2 text-left hover:bg-secondary"
-                                    onClick={handleEdit}
-                                >
-                                    Edit
-                                </button>
-                                <button
-                                    className="block w-full p-2 py-2 text-left  hover:bg-secondary"
-                                    onClick={handleDelete}
-                                >
-                                    Delete
-                                </button>
+            <div className="flex max-h-[300px] flex-col items-center justify-between rounded p-2">
+            <div className="relative rounded-lg border-2 border-background transition-all duration-300 ease-in-out hover:border-primary">
+                <div
+                    onClick={() => navigate(`/projects/${project._id}`)}
+                    className="relative rounded-lg"
+                >
+                    <img
+                        className="rounded-lg transition-all duration-300 ease-in-out hover:blur-sm"
+                        src={`${API_URL}/project/${project._id}/thumbnail`}
+                        alt={`Preview ${project.title}`}
+                    />
+                    <div className="absolute inset-0 flex items-end rounded-lg bg-gradient-to-t from-neutral-900 to-transparent opacity-0 transition-opacity duration-300 ease-in-out hover:opacity-100">
+                        <div className="relative w-full p-2 text-center">
+                            <div className="group relative flex items-center justify-center">
+                                {isEditing ? (
+                                    <div className="flex items-center">
+                                        <input
+                                            type="text"
+                                            value={newTitle}
+                                            onChange={handleTitleChange}
+                                            onClick={(e) => e.stopPropagation()}
+                                            onKeyDown={(e) => {
+                                                if (e.key === 'Enter') {
+                                                    handleTitleSave();
+                                                } else if (e.key === 'Escape') {
+                                                    handleTitleBlur();
+                                                }
+                                            }}
+                                            className="w-[100px] text-center"
+                                            autoFocus
+                                        />
+                                        <CheckIcon
+                                            className="absolute right-4 h-5 w-5 text-white"
+                                            onClick={(e) => {
+                                                e.stopPropagation();
+                                                handleTitleSave();
+                                            }}
+                                        />
+                                        <Cross1Icon
+                                            className="absolute right-0 h-4 w-4 text-white"
+                                            onClick={(e) => {
+                                                e.stopPropagation();
+                                                handleTitleBlur();
+                                            }}
+                                        />
+                                    </div>
+                                ) : (
+                                    <span className="text-white">
+                                        {project.title}
+                                    </span>
+                                )}
+                                <div className="absolute right-0 ml-2 opacity-0 transition-opacity group-hover:opacity-100">
+                                    <div
+                                        className="z-20 cursor-pointer"
+                                        onClick={(e) => {
+                                            e.stopPropagation(); // Добавьте этот вызов, чтобы предотвратить всплывание события
+                                            onMenuToggle();
+                                        }}
+                                    >
+                                        {isEditing ? null : (
+                                            <PenIcon className="h-3 w-3 text-white" />
+                                        )}
+                                    </div>
+                                    {isMenuVisible && (
+                                        <div
+                                            className="w-21 absolute right-0 top-full z-30 rounded-lg border bg-background text-sm text-foreground shadow-md"
+                                            onClick={(e) => e.stopPropagation()}
+                                        >
+                                            <button
+                                                className="block w-full p-2 text-left hover:bg-secondary"
+                                                onClick={(e) => {
+                                                    e.stopPropagation();
+                                                    handleEdit();
+                                                }}
+                                            >
+                                                Edit
+                                            </button>
+                                            <button
+                                                className="block w-full p-2 py-2 text-left hover:bg-secondary"
+                                                onClick={(e) => {
+                                                    e.stopPropagation();
+                                                    handleDelete();
+                                                }}
+                                            >
+                                                Delete
+                                            </button>
+                                        </div>
+                                    )}
+                                </div>
                             </div>
-                        )}
+                        </div>
                     </div>
                 </div>
             </div>
